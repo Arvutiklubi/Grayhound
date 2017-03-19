952abc801006d482822fa692cfc652a6ebb8f9f5
Joonatan Samuel <Joonatan.Samuel@starship.xyz>
2017-03-19 12:09:17 +0200

    Physics deltas

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+/**
+ * Created by koala on 19.03.17.
+ */
+public class Player extends PhysicsObject2D {
+
+    public float speed = 1f;
+
+    public Player(float x_, float y_)
+    {
+        // initialize position
+        super(x_, y_);
+    }
+
+}
